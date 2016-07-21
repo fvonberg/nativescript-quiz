9d9df4e7f20813e2fe873824f2db3a7a0b729c49
Frederik von Berg <vonberg@weiglewilczek.com>
2016-07-21 10:27:24 +0200

    start page animation

--- app/pages/home/home.component.ts
+++ app/pages/home/home.component.ts
@@ -13,6 +13,9 @@ import {AnimationDefinition} from "ui/animation";
     styleUrls: ["home-common.css", "home.css"]
 })
 export class HomeComponent implements OnInit {
+    @ViewChild("label") loginLabel: ElementRef;
+    @ViewChild("button") loginButton: ElementRef;
+
     constructor(private page: Page, private _router: Router) {}
 
     public ngOnInit() {
@@ -20,6 +23,19 @@ export class HomeComponent implements OnInit {
     }
 
     public startGame() {
+        let labelElement = <Label>this.loginLabel.nativeElement;
+        let loginButton = <Button>this.loginButton.nativeElement;
+        let animations: Array<AnimationDefinition> = [];
+
+        animations.push({target: labelElement, translate: {x: 400, y: 0}, duration: 1000});
+        animations.push({target: loginButton, translate: {x: -400, y: 0}, duration: 1000});
+
+        new Animation(animations, false).play().then(() => {
+            this.navigate();
+        });
+    }
+
+    private navigate() {
         this._router.navigate(["/game"]);
     }
 }
